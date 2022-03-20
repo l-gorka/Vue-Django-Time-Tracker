d91b616b7c397c05e1b39e1c1d5b482eb1c6a5c9
l-gorka <gorka.lukasz88@gmail.com>
2022-03-20 22:43:31 +0100

    Removed migrations.

--- timeTracker/settings.py
+++ timeTracker/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'search',
+        'NAME': 'trackerrr',
         'USER': 'postgres',
-        'PASSWORD': 'Pass2020!',
+        'PASSWORD': 'somePassword',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
