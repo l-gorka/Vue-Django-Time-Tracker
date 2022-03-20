d4a85f7b8049af9c2a869bdc46ab31d2d55d40e8
l-gorka <gorka.lukasz88@gmail.com>
2022-03-20 21:48:37 +0100

    Removed secret key.

--- timeTracker/settings.py
+++ timeTracker/settings.py
@@ -24,10 +24,10 @@ FIXTURE_DIRS = [
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2r0-em5**(by06f+&y5qj#3zs^s3^47hhci$o%p4fy@6(ttj(&'
+# SECRET_KEY = 'django-insecure-2r0-em5**(by06f+&y5qj#3zs^s3^47hhci$o%p4fy@6(ttj(&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
