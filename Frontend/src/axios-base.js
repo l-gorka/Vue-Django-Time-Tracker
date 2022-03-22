99569b9036ebf081024488b99471d25080eff4f4
l-gorka <gorka.lukasz88@gmail.com>
2022-03-22 17:36:07 +0100

    Set to local mode.

--- Frontend/src/axios-base.js
+++ Frontend/src/axios-base.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import jwtDecode from 'jwt-decode';
 import store from './store';
-const APIUrl = 'https://limitless-refuge-32819.herokuapp.com';
+const APIUrl = 'http://127.0.0.1:8000';
 import { SnackbarProgrammatic as snackbar } from "buefy";
 
 const axiosBase = axios.create({
