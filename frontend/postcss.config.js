db6084e3454a251621a84cf8af87dc904750226c
l-gorka <gorka.lukasz88@gmail.com>
2022-03-26 20:07:28 +0100

    Disabled purgecss.

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,3 +1,5 @@
+// 
+
 // const purgecss = require("@fullhuman/postcss-purgecss");
 
 // const plugins = [];
@@ -5,44 +7,21 @@
 // if (process.env.NODE_ENV === "production") {
 //     plugins.push(
 //         purgecss({
-//             content: ['./**/*.html'],
+//             content: [
+//               "./src/**/*.html", 
+//               "./src/**/*.vue",
+//               "./src/**/*.js",
+//               "./node_modules/buefy/**/*.vue",
+//               "./node_modules/buefy/**/*.js",
+//               "./node_modules/buefy/**/*.scss",
+//             ],
 //             safelist: {
-//                 standard: [/red$/],
-//                 deep: [/is$/, /hero$/, /has$/, /column$/, /container$/, /button$/, /hero$/,
-//                  /section$/, /carousel$/, /navbar$/, /navbar-item$/, /section$/, /panel$/,
-//                  /pt$/, /mt$/, /has-navbar$/
-//                 ]
-//             },
-//             whitelistPatterns: [/^navbar-/, /^panel/, /^has-text-/, /^has-/, /^fa-/, /^has-numberinput-/],
-//             whitelistPatternsChildren: [/^token/, /^pre/, /^code/],
+//                 standard: ['navbar-start', 'navbar-end', 'animation-content',
+//                  'enter-active-class', 'leave-active-class', 'carousel-item', 'fade-out', 'fade-in'],
+//                 deep: [/fade$/, /zoom$/, /slide$/]
+//             }
 //         })
-//     );
+//     )
 // }
 
 // module.exports = { plugins };
-
-const purgecss = require("@fullhuman/postcss-purgecss");
-
-const plugins = [];
-
-if (process.env.NODE_ENV === "production") {
-    plugins.push(
-        purgecss({
-            content: [
-              "./src/**/*.html", 
-              "./src/**/*.vue",
-              "./src/**/*.js",
-              "./node_modules/buefy/**/*.vue",
-              "./node_modules/buefy/**/*.js",
-              "./node_modules/buefy/**/*.scss",
-            ],
-            safelist: {
-                standard: ['navbar-start', 'navbar-end', 'animation-content',
-                 'enter-active-class', 'leave-active-class', 'carousel-item', 'fade-out', 'fade-in'],
-                deep: [/fade$/, /zoom$/, /slide$/]
-            }
-        })
-    )
-}
-
-module.exports = { plugins };
